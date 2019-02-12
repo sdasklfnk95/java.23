2ca2a71389175916e7b345b7f0b59f678d72b208
sdasklfnk95 <43901526+sdasklfnk95@users.noreply.github.com>
2019-02-12 10:20:32 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,14 @@
+
+public class main
+{
+  public static void main(String args[])
+  {
+    int num = 3;
+    if ( num % 2 ==0 )
+        System.out.println("even");
+        else if(num <0)
+        System.out.println("Invalid");
+     else
+        System.out.println("odd");
+  }
+}
